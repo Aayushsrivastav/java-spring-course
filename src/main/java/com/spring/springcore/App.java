afdefc495cd76f8b022a614f01d8e1ee3f2b580a
Aayushsrivastav <aayushs2797@gmail.com>
2021-02-23 10:10:30 +0530

    xml configuration setup

--- src/main/java/com/spring/springcore/App.java
+++ src/main/java/com/spring/springcore/App.java
@@ -1,5 +1,8 @@
 package com.spring.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student student1 = (Student) context.getBean("student1");
+
+        Student student2 = (Student) context.getBean("student2");
+        
+        System.out.println(student1);
+        System.out.println(student2);
     }
 }
