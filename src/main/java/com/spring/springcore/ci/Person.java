086199396815e7131e988a992bdd476e10caebe7
Aayushsrivastav <aayushs2797@gmail.com>
2021-02-26 09:50:20 +0530

    constructor injection using c schema

--- src/main/java/com/spring/springcore/ci/Person.java
+++ src/main/java/com/spring/springcore/ci/Person.java
@@ -1,15 +1,23 @@
 package com.spring.springcore.ci;
 
+import java.util.List;
+
 public class Person {
 	private String name;
 	private int personId;
 	private Certi certi;
+	private List<String> list;
 	
 	public Person(String name,int personId,Certi certi) {
 		this.name = name;
 		this.personId = personId;
 		this.certi = certi;
 	}
+
+//	@Override
+//	public String toString() {
+//		return "Person [name=" + name + ", personId=" + personId + ", certi=" + certi + ", list=" + list + "]";
+//	}
 
 	@Override
 	public String toString() {
